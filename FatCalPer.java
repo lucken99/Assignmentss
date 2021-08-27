cc69b5e7e44769319c516f741e72a2925e1e1045
Lalit Singh <lalit.singh01@nagarro.com>
2021-08-27 12:52:27 +0530

    added extra logic and single try catch

--- FatCalPer.java
+++ FatCalPer.java
@@ -2,15 +2,35 @@ import java.util.Scanner;
 
 public class FatCalPer {
 
-    public static void takeInput(){
+    public static String takeInput(){
         Scanner sc = new Scanner(System.in);
-        
+        String inp = sc.nextLine();
+        return inp;
     }
 
     public static void inputPrompt(String s){
         System.out.println(s);
     }
+
+    public static fatCalPercent()
     public static void main(String[] args) {
-        
+        FatCalPer ft = new FatCalPer();
+
+        ft.inputPrompt("Enter the name of a food item: ");
+        String name = ft.takeInput();
+
+        ft.inputPrompt("Enter the grams of fat: ");
+        try {
+            double gramsFat = Double.parseDouble(ft.takeInput());
+        }
+        catch (Exception e){
+            System.out.println("Enter Valid Input")
+        }
+
+        ft.inputPrompt("Enter the number of calories: ");
+        int cal = Integer.parseInt(ft.takeInput());
+
+
+
     }
 }
