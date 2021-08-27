c6c313ad2a721434967f39ab0a482665e4cf3616
Lalit Singh <lalit.singh01@nagarro.com>
2021-08-27 13:27:40 +0530

    after succesfull running simple program, need to include extra things like access modifiers, good exception handling and OOPs concept.

--- FatCalPer.java
+++ FatCalPer.java
@@ -12,7 +12,10 @@ public class FatCalPer {
         System.out.println(s);
     }
 
-    public static fatCalPercent()
+    public static double fatCalPercent(double gramsFat, int cal){
+
+        return (gramsFat * 9 * 100) / cal;
+    }
     public static void main(String[] args) {
         FatCalPer ft = new FatCalPer();
 
@@ -20,17 +23,33 @@ public class FatCalPer {
         String name = ft.takeInput();
 
         ft.inputPrompt("Enter the grams of fat: ");
+        double gramsFat=0;
         try {
-            double gramsFat = Double.parseDouble(ft.takeInput());
+            gramsFat = Double.parseDouble(ft.takeInput());
         }
         catch (Exception e){
-            System.out.println("Enter Valid Input")
+            System.out.println("Enter Valid Input");
+            System.exit(1);
         }
 
         ft.inputPrompt("Enter the number of calories: ");
-        int cal = Integer.parseInt(ft.takeInput());
+        int cal=0;
+        try {
+            cal = Integer.parseInt(ft.takeInput());
+        }
+        catch (Exception e){
+            System.out.println("Enter Valid input of number of calories.");
+            System.exit(1);
+        }
 
-
+        double perCal = ft.fatCalPercent(gramsFat, cal);
+        System.out.printf("%s: %.2f\n",name, perCal);
+        if(perCal > 30){
+            System.out.println("This item is NOT Heart Healthy!");
+        }
+        else {
+            System.out.println("This item is Heart Healthy!");
+        }
 
     }
 }
