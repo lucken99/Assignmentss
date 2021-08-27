3f3d5cbe2d297877031089743c758836873ca25c
Lalit Singh <lalit.singh01@nagarro.com>
2021-08-27 12:22:35 +0530

    Created main, takeInput and inputPrompt method

---
+++ FatCalPer.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FatCalPer {
+
+    public static void takeInput(){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+
+    public static void inputPrompt(String s){
+        System.out.println(s);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
