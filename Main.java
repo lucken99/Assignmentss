3f84d5c9cd6c15794859253a0af98d8be1afaa08
lalitsingh01 <lalit.singh01@nagarro.com>
2021-09-01 17:30:36 +0530

    make FoodItem class immutable by using final and removing setters and creating a constructor

--- Main.java
+++ Main.java
@@ -1,36 +1,41 @@
 import java.util.Scanner;
 
-class FoodItem{
+final class FoodItem{
 
-	private String foodName;
-	private double fatGrams;
-	private int calories;
+	final private String foodName;
+	final private double fatGrams;
+	final private int calories;
 
+	public FoodItem(String foodName, double fatGrams, int calories){
+		this.foodName = foodName;
+		this.fatGrams = fatGrams;
+		this.calories = calories;
+	}
 	public String getFoodName(){
 		return this.foodName;
 	}
 
-	public void setFoodName(String foodName){
-		this.foodName = foodName;
-	}
+//	public void setFoodName(String foodName){
+//		this.foodName = foodName;
+//	}
 
 
 	public double getFatGrams(){
 		return this.fatGrams;
 	}
 
-	public void setFatGrams(double fatGrams){
-		this.fatGrams = fatGrams;
-	}
+//	public void setFatGrams(double fatGrams){
+//		this.fatGrams = fatGrams;
+//	}
 
 
 	public int getCalories(){
 		return this.calories;
 	}
 
-	public void setCalories(int calories){
-		this.calories = calories;
-	}
+//	public void setCalories(int calories){
+//		this.calories = calories;
+//	}
 
 
 	public double calculateFatCalPercent(){
@@ -55,17 +60,17 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		FoodItem foodObj = new FoodItem();
-		try{
-			foodObj.setFoodName(getInput("Enter the name of a food item: "));
-			foodObj.setFatGrams(Double.parseDouble(getInput("Enter the grams of fat in the "
-									+foodObj.getFoodName()+": ")));
-			foodObj.setCalories(Integer.parseInt(getInput("Enter the number of calories in "
-									+foodObj.getFoodName()+": ")));
-		}catch(Exception e){
-			System.out.println("Invalid input");
-			System.exit(1);
-		}
+//		FoodItem foodObj = new FoodItem();
+
+//			foodObj.setFoodName(getInput("Enter the name of a food item: "));
+//			foodObj.setFatGrams(Double.parseDouble(getInput("Enter the grams of fat in the "
+//									+foodObj.getFoodName()+": ")));
+//			foodObj.setCalories(Integer.parseInt(getInput("Enter the number of calories in "
+//									+foodObj.getFoodName()+": ")));
+		String foodName = getInput("Enter the name of a food item: ");
+		double fatGrams = Double.parseDouble(getInput("Enter the grams of fat: "));
+		int calories = Integer.parseInt(getInput("Enter the number of calories: "));
+		FoodItem foodObj = new FoodItem(foodName, fatGrams, calories);
 
 		double fatCalPer = foodObj.calculateFatCalPercent();
 		System.out.println(foodObj.getFoodName()+" has "
