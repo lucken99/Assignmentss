8f53b21fb7c046f149604d377181d6ce106ed06e
lalitsingh01 <lalit.singh01@nagarro.com>
2021-09-22 16:33:54 +0530

    changed directory structure

---
+++ Java_Core1/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+	private static String getInput(String prompt){
+		Scanner sc = new Scanner(System.in);
+		System.out.println(prompt);
+		return sc.nextLine();
+	}
+
+	public static void main(String[] args) {
+		String foodName = getInput("Enter the name of a food item: ");
+		double fatInGrams = Double.parseDouble(getInput("Enter the grams of fat: "));
+		int totalCalories = Integer.parseInt(getInput("Enter the number of calories: "));
+
+		FoodItem foodObj = new FoodItem(foodName, fatInGrams, totalCalories);
+
+		System.out.println(foodObj.getFoodName()+" has "
+							+String.format("%.2f", foodObj.calculateFatCalPercent())
+							+" % of calories that comes from fat.");
+
+		if(foodObj.isHealthyFatPercent()){
+			System.out.println("This item is Heart Healthy!");
+		}
+		else{
+			System.out.println("This item is NOT Heart Healthy!");
+		}
+
+	}
+}
