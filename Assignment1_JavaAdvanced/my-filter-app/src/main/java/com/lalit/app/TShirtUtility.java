43a90a2b8e33cc6f3fac060a1cf801477d669de5
lalitsingh01 <lalit.singh01@nagarro.com>
2021-09-29 04:16:40 +0530

    created separate methods for loading and searching Tshirts files and TShirts and also added comparators based on price and rating

--- Assignment1_JavaAdvanced/my-filter-app/src/main/java/com/lalit/app/TShirtUtility.java
+++ Assignment1_JavaAdvanced/my-filter-app/src/main/java/com/lalit/app/TShirtUtility.java
@@ -7,38 +7,78 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TShirtUtility {
-    public static void searchTShirts(String colour, String size, String gender) {
-        String folderName = "src\\main\\resources\\csv_files\\";
+
+    private static final String FOLDER_PATH = "src\\main\\resources\\csv_files\\";
+
+    private static List<String> getCSVFiles() {
         List<String> csvFiles = new ArrayList<>();
-        try (Stream<Path> walk = Files.walk(Paths.get(folderName))) {
+        try (Stream<Path> walk = Files.walk(Paths.get(FOLDER_PATH))) {
             csvFiles = walk.map(Path::toString)
                     .filter(f -> f.endsWith(".csv")).collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return csvFiles;
+    }
 
-        for (String fileName: csvFiles) {
+    public static List<TShirt> searchTShirts(String colour, String size, String gender) {
+        List<TShirt> resultingTShirts = new ArrayList<>();
+        for (String fileName: getCSVFiles()) {
             try (FileReader fr = new FileReader(fileName)) {
-                List<TShirt> resultingTShirts = new CsvToBeanBuilder<TShirt>(fr)
+                 resultingTShirts = new CsvToBeanBuilder<TShirt>(fr)
                         .withSeparator('|')
                         .withSkipLines(1)
                         .withType(TShirt.class)
                         .build()
-                        .stream().filter(x -> x.getAvailability().equalsIgnoreCase("Y")
-                        && (x.getColour().equalsIgnoreCase(colour)
-                        && x.getSize().equalsIgnoreCase(size)
-                        && x.getGenderRecommended().equalsIgnoreCase(gender))).collect(Collectors.toList());
-                resultingTShirts.forEach(System.out::println);
+                        .stream().filter(getCondition(colour, size, gender))
+                         .collect(Collectors.toList());
             } catch (IOException e) {
                     e.printStackTrace();
             }
-
         }
-
+        return resultingTShirts;
     }
+
+    private static Predicate<TShirt> getCondition(String colour, String size, String gender) {
+        return x -> x.getAvailability().equalsIgnoreCase("Y")
+                && x.getColour().equalsIgnoreCase(colour)
+                && x.getSize().equalsIgnoreCase(size)
+                && x.getGenderRecommended().equalsIgnoreCase(gender);
+    }
+
+    public static void orderBy(List<TShirt> tShirts, String preference) {
+        if (preference.equalsIgnoreCase("price")) {
+            Collections.sort(tShirts, new PriceComparator());
+        }
+        else if (preference.equalsIgnoreCase("rating")) {
+            Collections.sort(tShirts, new RatingComparator());
+        } else if (preference.equalsIgnoreCase("both")
+                        || preference.equalsIgnoreCase("price and rating")) {
+            Collections.sort(tShirts, new PriceComparator().thenComparing(new RatingComparator()));
+        }
+    }
+    public static class PriceComparator implements Comparator<TShirt> {
+
+        @Override
+        public int compare(TShirt tShirt1, TShirt tShirt2) {
+            return Double.compare(tShirt1.getPrice(), tShirt2.getPrice());
+        }
+    }
+
+    public static class RatingComparator implements Comparator<TShirt> {
+
+        @Override
+        public int compare(TShirt tShirt1, TShirt tShirt2) {
+            return Double.compare(tShirt1.getRating(), tShirt2.getRating());
+        }
+    }
+
 }
