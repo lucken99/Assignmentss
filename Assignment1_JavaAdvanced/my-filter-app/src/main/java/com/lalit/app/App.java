b5b747d45b1b27f82c8052ca422379c85fdeb8a2
lalitsingh01 <lalit.singh01@nagarro.com>
2021-09-28 14:14:23 +0530

    created the working program without using threads

--- Assignment1_JavaAdvanced/my-filter-app/src/main/java/com/lalit/app/App.java
+++ Assignment1_JavaAdvanced/my-filter-app/src/main/java/com/lalit/app/App.java
@@ -1,14 +1,30 @@
 package com.lalit.app;
 
+import java.util.Scanner;
+
+import static com.lalit.app.TShirtUtility.searchTShirts;
+
 /**
- * Hello world!
- *
+ * Author {
+ *     name = "Lalit Singh",
+ *     date = "26-09-2021"
+ *         }
  */
+
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
 
-        System.out.println( "Hello World!" );
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Color of T-Shirt to search: ");
+        String color = sc.nextLine();
+        System.out.println("Enter the Size(S, M, L, XL) of T-Shirt: ");
+        String size = sc.nextLine();
+        System.out.println("Enter the gender(M, F) preference: ");
+        String gender = sc.nextLine();
+        System.out.println("Output Preference (for e.g., by Price, by Rating or by Price and Rating: ");
+        String outputPreference = sc.nextLine();
+
+        searchTShirts(color, size, gender);
     }
 }
