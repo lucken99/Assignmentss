3b23959ae4a9ca7ff247106dfd256e27d6da0e31
lalitsingh01 <lalit.singh01@nagarro.com>
2021-10-12 14:37:17 +0530

    Update HibernateUserUtil.java

--- Assignment3_JavaAdvanced/Assignment3/src/main/java/com/lalit/util/HibernateUserUtil.java
+++ Assignment3_JavaAdvanced/Assignment3/src/main/java/com/lalit/util/HibernateUserUtil.java
@@ -20,7 +20,7 @@ public class HibernateUserUtil {
                 // Hibernate settings equivalent to hibernate.cfg.xml's properties
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, "jdbc://mysql://localhost:3306/ebookshop");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/ebookshop");
                 settings.put(Environment.USER, "root");
                 settings.put(Environment.PASS, "root");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
